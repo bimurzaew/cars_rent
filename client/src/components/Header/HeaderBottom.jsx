d6fbc10412fde917a9429cea03b7fcf779a82997
Khatciev <killermen095@mail.ru>
2021-09-09 17:43:09 +0300

    Не доделал поиск

--- client/src/components/Header/HeaderBottom.jsx
+++ client/src/components/Header/HeaderBottom.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
-import {getByCategories, loadCars} from "../../redux/features/cars";
+import { getByCategories, loadCars, searchCars } from "../../redux/features/cars";
+import { TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles({
+  search: {
+    margin: "15px",
+    backgroundColor: "white"
+  }
+})
+
 
 function HeaderBottom() {
-    const { categories } = useSelector((state) => state.categories);
 
+    const { categories } = useSelector((state) => state.categories);
     const dispatch = useDispatch()
+    const classes = useStyles()
 
     const handleGetByCategories = (id) => {
         dispatch(getByCategories(id))
@@ -15,16 +27,21 @@ function HeaderBottom() {
     const handleLoadCars = () => {
         dispatch(loadCars())
     }
+    const handleSearch = (e) => {
+      dispatch(searchCars(e.target.value))
+    }
 
     return (
         <>
-            <div className="col">
+
+            <div className="main-nav">
+              <div className="col">
                 <p onClick={handleLoadCars}  className="head-nav">
-                    <NavLink exact to="/">
-                        Главная
-                    </NavLink>
+                  <NavLink exact to="/">
+                    Главная
+                  </NavLink>
                 </p>
-            </div>
+              </div>
             {categories.map(item => {
 
                 return(
@@ -39,7 +56,14 @@ function HeaderBottom() {
 
                 )
             })}
-
+              <TextField
+                onChange={handleSearch}
+                className={classes.search}
+                label="Поиск"
+                type="search"
+                variant="filled"
+              />
+            </div>
         </>
     );
 }
