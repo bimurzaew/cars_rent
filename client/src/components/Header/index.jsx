7983df452539fc6e19900acf6c2a4b6223388768
bimurzaew <85867193+bimurzaew@users.noreply.github.com>
2021-10-18 17:41:16 +0300

    header

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -51,7 +51,7 @@ function Header(props) {
               </div>
               <div className="col-6">
                   <div className={classes.text_title}>
-                    Ахьмадс ишт мег аьлла
+                    Подберем для вас автомобиль
                   </div>
               </div>
               <div className="col">
